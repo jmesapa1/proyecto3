34b8d84ae5ca1933afb02b67569ed231b8b31633
jmesapa1 <43121663+jmesapa1@users.noreply.github.com>
2019-05-09 00:32:01 -0500

    Update Contador.py

--- Contador.py
+++ Contador.py
@@ -2,10 +2,14 @@ import operator
 Buscar="house"
 Buscar=Buscar.lower()
 released = {
-    0 : 0}
+		0 : 0}
+releasedTitulo = {
+		0 : 0}
+contadorLinea=0
 for linea in LineFiltered:
   Id=""
   contador=0
+  
   frecuencia=0
   coleccionPalabras=tokenizer.tokenize(linea)
   for palabra in coleccionPalabras:
@@ -14,9 +18,21 @@ for linea in LineFiltered:
       frecuencia+=1
     if contador==2:
       Id=palabra
+
       
-      
+  releasedTitulo[tittle[contadorLinea]]=frecuencia    
   released[Id]=frecuencia
+  contadorLinea+=1
+
+ordenTitulo=sorted(releasedTitulo.items(),key=operator.itemgetter(1))  
+  
 sorted_x = sorted(released.items(), key=operator.itemgetter(1))
 sorted_x.reverse()
-print (sorted_x)    
+ordenTitulo.reverse()
+
+i=0
+while i < 5:
+  print( sorted_x[i])
+  print(ordenTitulo[i])
+  print("------------------------------------------------------------")
+  i += 1   
